86cb80886cbf1f0f1eb86749721e8117a6cec1d6
Ameer67 <ameerzaita9@gmail.com>
2019-07-29 15:20:05 -0500

    Added calculation functions

--- calc.js
+++ calc.js
@@ -3,13 +3,13 @@
 //          amout of profit
 // margin = what % of total revenue is profit
 
-// input variables
+// INPUT VARIABLES
 let soldPrice = document.getElementById("sold-price");
 let shippingPrice = document.getElementById("shipping-price");
 let itemCost = document.getElementById("item-cost");
 let shippingCost = document.getElementById("shipping-cost");
 
-// output variables
+// OUTPUT VARIABLES
 let revenue = document.getElementById("revenue");
 let ebayFee = document.getElementById("ebay-fee");
 let paypalFee = document.getElementById("paypal-fee");
@@ -18,12 +18,23 @@ let totalProfit = document.getElementById("total-profit");
 let returnPercentage = document.getElementById("return");
 let marginPercentage = document.getElementById("margin");
 
-// function calculateEbayFee(revenue)
+// CALCULATIONS
+function calculateEbayFee(revenue) {
+    return (.10 * revenue).toFixed(2);
+}
 
-// function calculatePayPalFee(revenue)
+function calculatePayPalFee(revenue) {
+    return ((.029 * revenue) + .30).toFixed(2);
+}
 
-// function calculateTotalProfit(revenue, costs)
+function calculateTotalProfit(revenue, costs) {
+    return (revenue - costs).toFixed(2);
+}
 
-// function calculateReturn(profit, cost)
+function calculateReturnPercentage(profit, costs) {
+    return ((parseFloat(profit) / costs) * 100).toFixed(1);
+}
 
-// function calculateMargin(profit, revenue)
+function calculateMarginPercentage(profit, revenue) {
+    return ((parseFloat(profit) / revenue) * 100).toFixed(1);
+}
