3418a5141258566da1993b5672a6d81c6795c4a8
Ameer67 <ameerzaita9@gmail.com>
2019-07-29 14:37:21 -0500

    Added calculator input and output variables

--- calc.js
+++ calc.js
@@ -1,3 +1,29 @@
+// DEFINTIONS:
 // return = % of cost that became the
 //          amout of profit
 // margin = what % of total revenue is profit
+
+// input variables
+let soldPrice = document.getElementById("sold-price");
+let shippingPrice = document.getElementById("shipping-price");
+let itemCost = document.getElementById("item-cost");
+let shippingCost = document.getElementById("shipping-cost");
+
+// output variables
+let revenue = document.getElementById("revenue");
+let ebayFee = document.getElementById("ebay-fee");
+let paypalFee = document.getElementById("paypal-fee");
+let costs = document.getElementById("costs");
+let totalProfit = document.getElementById("total-profit");
+let returnPercentage = document.getElementById("return");
+let marginPercentage = document.getElementById("margin");
+
+// function calculateEbayFee(revenue)
+
+// function calculatePayPalFee(revenue)
+
+// function calculateTotalProfit(revenue, costs)
+
+// function calculateReturn(profit, cost)
+
+// function calculateMargin(profit, revenue)
