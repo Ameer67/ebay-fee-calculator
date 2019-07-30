89b97e2231386a22dcd1f7f9199f203b36ac7212
Ameer67 <ameerzaita9@gmail.com>
2019-07-29 22:47:47 -0500

    Removed multiple functions to calculate results and instead replaced them with one function to calculate and print results.

--- calc.js
+++ calc.js
@@ -3,13 +3,43 @@
 //          amout of profit
 // margin = what % of total revenue is profit
 
-// INPUT VARIABLES
+// CALCULATE AND OUTPUT RESULTS
+function printResults() {
+    // REVENUE
+    let revenueAmount = (parseFloat(soldPrice.value) + parseFloat(shippingPrice.value)).toFixed(2);
+    revenue.value = revenueAmount;
+
+    // FEES
+    ebayFee.value = (0.10 * revenueAmount).toFixed(2);
+    paypalFee.value = (.029 * revenueAmount + 0.30).toFixed(2);
+
+    // COSTS
+    let costsAmount = (parseFloat(itemCost.value) + parseFloat(shippingCost.value) + parseFloat(ebayFee.value) + parseFloat(paypalFee.value)).toFixed(2);
+    costs.value = costsAmount;
+
+    // PROFIT
+    let profitAmount = (revenueAmount - costsAmount).toFixed(2);
+    totalProfit.value = profitAmount;
+
+    // RETURN %
+    let returnAmount = ((profitAmount / costsAmount) * 100).toFixed(1);
+    returnPercentage.value = returnAmount;
+
+    // MARGIN %
+    let marginAmount = ((profitAmount / revenueAmount) * 100).toFixed(1);
+    marginPercentage.value = marginAmount;
+}
+
+// MAIN CONTAINER
+let calculatorContainer = document.getElementById("calculator-container");
+
+// HTML INPUT VARIABLES
 let soldPrice = document.getElementById("sold-price");
 let shippingPrice = document.getElementById("shipping-price");
 let itemCost = document.getElementById("item-cost");
 let shippingCost = document.getElementById("shipping-cost");
 
-// OUTPUT VARIABLES
+// HTML OUTPUT VARIABLES
 let revenue = document.getElementById("revenue");
 let ebayFee = document.getElementById("ebay-fee");
 let paypalFee = document.getElementById("paypal-fee");
@@ -18,23 +48,5 @@ let totalProfit = document.getElementById("total-profit");
 let returnPercentage = document.getElementById("return");
 let marginPercentage = document.getElementById("margin");
 
-// CALCULATIONS
-function calculateEbayFee(revenue) {
-    return (.10 * revenue).toFixed(2);
-}
-
-function calculatePayPalFee(revenue) {
-    return ((.029 * revenue) + .30).toFixed(2);
-}
-
-function calculateTotalProfit(revenue, costs) {
-    return (revenue - costs).toFixed(2);
-}
-
-function calculateReturnPercentage(profit, costs) {
-    return ((parseFloat(profit) / costs) * 100).toFixed(1);
-}
-
-function calculateMarginPercentage(profit, revenue) {
-    return ((parseFloat(profit) / revenue) * 100).toFixed(1);
-}
+// USER INTERFACE 
+calculatorContainer.addEventListener("input", printResults);
